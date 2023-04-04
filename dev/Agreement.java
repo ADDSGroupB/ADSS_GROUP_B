c89799b1994df64591b9a86aca592d677286a232
Guy Biton <guybito@post.bgu.ac.il>
2023-04-04 17:55:42 +0300

    class v1

--- dev/Agreement.java
+++ dev/Agreement.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 public class Agreement
@@ -7,6 +9,29 @@ public class Agreement
     private int agreementID;
     private String paymentType;
     private Map<Integer, ArrayList<Discount>> discountProducts;
+    private ArrayList<Discount> totalOrderDiscount;
+    private boolean selfSupply;
+    private ArrayList<DaysOfWeek> supplyDays;
 
-
+    public Agreement(String paymentType, boolean selfSupply, ArrayList<DaysOfWeek> supplyDays) {
+        this.agreementID = id++;
+        this.paymentType = paymentType;
+        this.discountProducts = new HashMap<Integer, ArrayList<Discount>>();
+        this.totalOrderDiscount = new ArrayList<Discount>();
+        this.selfSupply = selfSupply;
+        this.supplyDays = supplyDays;
+    }
+    public void addTotalDiscount(String type, int quantity, int value)
+    {
+        if (type.toLowerCase() == "Percent")
+            totalOrderDiscount.add(new PercentDiscount(quantity, value));
+        else if (type.toLowerCase() == "Value")
+            totalOrderDiscount.add(new ValueDiscount(quantity, value));
+        else
+            System.out.println("Wrong type, please Choose Percent / Value");
+    }
+    public void addProductDiscount(String type, int productID, int quantity, int value)
+    {
+//        discountProducts[productID].
+    }
 }
--- dev/Supplier.java
+++ dev/Supplier.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Supplier
 {
+    private static int id = 0;
     private int supplierID;
     private String firstName;
     private String lastName;
@@ -13,4 +15,28 @@ public class Supplier
     private boolean isActive;
     private Map<Integer, Integer> products;
     private Map<Integer, Integer> productsAmount;
+    private Agreement agreement;
+
+    public Supplier(String firstName, String lastName, String bankAccount, ArrayList<String> manufacturers, ArrayList<String> domains) {
+        this.supplierID = id++;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.contact = new HashMap<String, String>();
+        this.bankAccount = bankAccount;
+        this.manufacturers = manufacturers;
+        this.domains = domains;
+        this.isActive = true;
+        this.products = new HashMap<Integer, Integer>();;
+        this.productsAmount  = new HashMap<Integer, Integer>();;
+        this.agreement = null;
+    }
+
+    public void setAgreement(String paymentType, boolean selfSupply, ArrayList<DaysOfWeek> supplyDays) {
+        this.agreement = new Agreement(paymentType, selfSupply, supplyDays);
+    }
+    public void addTotalDiscount(String type, int quantity, int value)
+    {
+        this.agreement.addTotalDiscount(type, quantity, value);
+    }
+
 }
