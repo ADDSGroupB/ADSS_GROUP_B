2899302e8d7453ba1c019a808068f91722c996b4
Dan Vaitzman <danvai@post.bgu.ac.il>
2023-05-09 13:35:17 +0300

    Dan - 9.5 - 13:22 ADD TODO

--- dev/InterfaceLayer/Main.java
+++ dev/InterfaceLayer/Main.java
@@ -1,12 +1,20 @@
 package InterfaceLayer;
 
 import BusinessLayer.Product;
+import DataAccess.ProductsDao;
+import DataAccess.ProductsDaoImpl;
+
+import java.sql.SQLException;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Cli cli = new Cli();
-        cli.start();
+    public static void main(String[] args) throws SQLException {
+        ProductsDao productsDao = new ProductsDaoImpl();
+        Product product = productsDao.getProductByID(1);
+        System.out.println(product.toString());
+
+//        Cli cli = new Cli();
+//        cli.start();
 
 
     }
